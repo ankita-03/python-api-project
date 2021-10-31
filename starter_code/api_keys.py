3451e0dbaf2a10c4a614f4d58a4b6853e42cc45e
ankita-03 <ankita@gautam.us>
2021-10-30 23:50:39 -0500

    adding files

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "dda0f1a41bca5c1231bf3a09a1427a7b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBnQHmBHExOz7uy-7AtfoLrCSsi7Y2gnNM"
